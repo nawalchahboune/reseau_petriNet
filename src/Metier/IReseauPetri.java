f740fb4e7144c9827f3019ca8335445722b83dc8
Nawal CHAHBOUNE <nawalchahbouneimt@gmail.com>
2024-10-09 15:37:54 +0200

    classes definies

--- src/Metier/IReseauPetri.java
+++ src/Metier/IReseauPetri.java
@@ -1,5 +1,13 @@
 package Metier;
 
-public class IReseauPetri {
+public interface IReseauPetri {
+	public void ajouter_Arc(Arc arc);
+	public void supprimer_Arc(Arc arc);
+	public void ajouter_Place(Place place);
+	public void supprimer_Place(Place place);
+	public void ajouter_Tarnsition(Transition transition);
+	public void supprimer_Tarnsition(Transition transition);
+	public void fire(Transition transition);
+	public void fireAll();
 
 }
